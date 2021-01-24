f9b41040210de94025aa632d906591437eb34dd7
webdevandres <maciasandres02@gmail.com>
2021-01-24 15:19:44 -0600

    modified calcFutureValue to display results

--- calcValue.js
+++ calcValue.js
@@ -4,54 +4,27 @@ var $ = function (id) {
     return document.getElementById(id);
 };
 
-var createNodes = function(futureValue) {
-    var inputNode = document.createElement("input");
-    var labelNode = document.createElement("label");
-    //set up the properties and attributes of elements
-    inputNode.type = "text";
-    inputNode.id = "result";
-    inputNode.setAttribute("disabled", true);
-    inputNode.value = futureValue;
-    labelNode.textContent = "Future Value";
-    labelNode.setAttribute("for", "result");
-    //attach the nodes to the end of the section
-    var parentLabelDiv = document.getElementById("display_result_label");
-    var parentResultDiv = document.getElementById("display_result");
-    parentLabelDiv.appendChild(labelNode);
-    parentResultDiv.appendChild(inputNode);
-};
 
-var displayResult = function (futureValue) {
-    var resultElement = $("result");
-    var newValue = futureValue;
-    if(resultElement) {
-        var currentValue = resultElement.value;
-        if(currentValue !== newValue) {
-            resultElement.value = newValue;
-        } 
-    } else {
-        if(!isNaN(newValue)) {
-            createNodes(newValue);
-        }
-    }
-};
-
-var calcFutureValue = function (investment, rate, years) {
+var calcFutureValue = function (investment, rate, year) {
     var futureValue;
     futureValue = investment;
-    for (var i = 1; i <= years; i++) {
-        futureValue = futureValue + (futureValue * rate / 100);
-    }
-    futureValue = parseFloat(futureValue).toFixed(2);
-    return futureValue;
+    var yearlyInterest;
+    var html = "";
+    for (var i = 1; i <= year; i++) {
+        yearlyInterest = futureValue * (rate / 100);
+        futureValue = futureValue + yearlyInterest;
+        html +=" Year=" + i + " Interest=" + yearlyInterest.toFixed(2) + " Value=" + parseInt(futureValue) + "<br>";
+    };
+    
+    $("main_container").innerHTML = html;
 };
 
 var getUserEntries = function () {
     var investment = parseFloat($("investment").value);
     var rate = parseFloat($("rate").value);
     var year = parseFloat($("years").value);
-    var result = calcFutureValue(investment, rate, year);
-    displayResult(result);
+    calcFutureValue(investment, rate, year);
+    
 };
 
 
