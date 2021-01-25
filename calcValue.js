e2096754d2c89bb1e1ba336fe189d47e3c285d00
webdevandres <maciasandres02@gmail.com>
2021-01-24 20:19:32 -0600

    added validation

--- calcValue.js
+++ calcValue.js
@@ -6,28 +6,52 @@ var $ = function (id) {
 
 
 var calcFutureValue = function (investment, rate, year) {
-    var futureValue;
-    futureValue = investment;
-    var yearlyInterest;
-    var html = "";
-    for (var i = 1; i <= year; i++) {
-        yearlyInterest = futureValue * (rate / 100);
-        futureValue = futureValue + yearlyInterest;
-        html +=" Year=" + i + " Interest=" + yearlyInterest.toFixed(2) + " Value=" + parseInt(futureValue) + "<br>";
+    var futureValue = investment;
+        for (var i = 1; i <= year; i++) {
+        futureValue = futureValue + (futureValue * rate / 100);
     };
-    $("main_container").style.backgroundColor = "white";
-    $("main_container").innerHTML = html;
+    futureValue = parseFloat(futureValue).toFixed(2);
+    return futureValue;
 };
 
-var getUserEntries = function () {
+
+var processEntries = function () {
     var investment = parseFloat($("investment").value);
     var rate = parseFloat($("rate").value);
-    var year = parseFloat($("years").value);
-    calcFutureValue(investment, rate, year);
-    
+    var years = parseFloat($("years").value);
+    var isValid = true;
+
+    if (isNaN(investment) || investment <= 0 || investment >= 100000) {
+        $("investment_error").firstChild.nodeValue = "Must be a number greater than 0 and less than 100,000.";
+        isValid = false;
+    } else {
+        $("investment_error").firstChild.nodeValue = "";
+    }
+
+    if (isNaN(rate) || rate <= 0 || rate > 15) {
+        $("rate_error").firstChild.nodeValue = "Must be a number greater than 0 and less than or equal to 15.";
+        isValid = false;
+    } else {
+        $("rate_error").firstChild.nodeValue = "";
+    }
+
+    if (isNaN(years) || years <= 0 || years > 50) {
+        $("years_error").firstChild.nodeValue = "Must be a number greater than 0 and less than or equal to 50.";
+        isValid = false;
+    } else {
+        $("years_error").firstChild.nodeValue = "";
+    }
+
+    if (isValid) {
+        var futureValue = calcFutureValue(investment, rate, years);
+        $("result").value = futureValue;
+        $("investment").focus();
+    }
+
 };
 
 
 window.onload = function () {
-    $("calculate_value").onclick = getUserEntries;
+    $("calculate_value").onclick = processEntries;
+    $("investment").focus();
 };
